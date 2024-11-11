593aefb9b72561f6e06525ddc60816257ede643a
jocmom <jombi@posteo.net>
2024-11-11 21:44:18 +0100

    Serialization

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,12 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
